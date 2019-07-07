ef8077efdfd8e6a55dda30cc8742490efab76cbe
Alriosa <carlos.alfredo.rios15@gmail.com>
2019-07-06 22:38:29 -0600

    4

--- src/entities/SimpleBurguer.java
+++ src/entities/SimpleBurguer.java
@@ -1,5 +1,10 @@
 package entities;
 
-public class SimpleBurguer extends Burguer {
-
+public class SimpleBurguer extends Burguer{
+	public SimpleBurguer(String pName, String pType, String pIngredients) {
+		super(pName,pType,pIngredients);
+		this.setName("");
+		this.setType("");
+		this.setIngredients("");
+	}
 }
